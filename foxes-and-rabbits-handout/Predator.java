0b074c519ff749a42434fbcad92a13ea8833409f
MihailBratanov <m.d.bratanov@gmail.com>
2019-02-13 10:26:21 +0000

    Add files via upload
    This is the latest version of the project with the gender almost implemented.

---
+++ foxes-and-rabbits-handout/Predator.java
@@ -0,0 +1,252 @@
+import java.util.List;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.*;
+
+/**
+ * A simple model of a fox.
+ * Foxes age, move, eat rabbits, and die.
+ * 
+ * @author David J. Barnes and Michael Kölling
+ * @version 2016.02.29 (2)
+ */
+public abstract class Predator extends Animal
+{
+    // Characteristics shared by all foxes (class variables).
+    
+    // The age at which a fox can start to breed.
+    private int BREEDING_AGE;
+    // The age to which a fox can live.
+    private  int MAX_AGE;
+    // The likelihood of a fox breeding.
+    private double BREEDING_PROBABILITY;
+    // The maximum number of births.
+    private  int MAX_LITTER_SIZE;
+    // The food value of a single rabbit. In effect, this is the
+    // number of steps a fox can go before it has to eat again.
+    private  int BADGER_FOOD_VALUE;
+    private  int SQUIRREL_FOOD_VALUE;
+    // A shared random number generator to control breeding.
+    private  Random rand = Randomizer.getRandom();
+
+    
+    
+    // Individual characteristics (instance fields).
+    // The fox's age.
+    private int age;
+    // The fox's food level, which is increased by eating rabbits.
+
+    /**
+     * Create a fox. A fox can be created as a new born (age zero
+     * and not hungry) or with a random age and food level.
+     * 
+     * @param randomAge If true, the fox will have random age and hunger level.
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     */
+    public Predator(Field field, Location location)
+    {
+        super(field, location);
+        int b = getBadgerFoodLevel();
+        int s = getSquirrelFoodLevel();
+        int h = getHedgehogFoodLevel();
+       
+        
+    }
+    /**
+     * Check whether or not this rabbit is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newRabbits A list to return newly born rabbits.
+     */
+    abstract protected double getBreedingProbability();
+    /**
+     * Check whether or not this rabbit is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newRabbits A list to return newly born rabbits.
+     */
+    abstract protected int getMaxLitterSize();
+    /**
+     * Check whether or not this rabbit is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newRabbits A list to return newly born rabbits.
+     */
+    abstract protected int getAge();
+    /**
+     * Check whether or not this rabbit is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newRabbits A list to return newly born rabbits.
+     */
+    abstract protected int getMaxAge();
+    /**
+     * Check whether or not this rabbit is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newRabbits A list to return newly born rabbits.
+     */
+    abstract protected int getBreedingAge();
+    /**
+     * Check whether or not this rabbit is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newRabbits A list to return newly born rabbits.
+     */
+    abstract protected int getBadgerFoodValue();
+    /**
+     * Check whether or not this rabbit is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newRabbits A list to return newly born rabbits.
+     */
+    abstract protected int getSquirrelFoodValue();
+    /**
+     * Check whether or not this rabbit is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newRabbits A list to return newly born rabbits.
+     */
+    abstract protected int getHedgehogFoodValue();
+    /**
+     * Check whether or not this rabbit is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newRabbits A list to return newly born rabbits.
+     */
+    abstract protected int getBadgerFoodLevel();
+    /**
+     * Check whether or not this rabbit is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newRabbits A list to return newly born rabbits.
+     */
+    abstract protected int getSquirrelFoodLevel();
+    /**
+     * Check whether or not this rabbit is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newRabbits A list to return newly born rabbits.
+     */
+    abstract protected int getHedgehogFoodLevel();
+    /**
+     * This is what the fox does most of the time: it hunts for
+     * rabbits. In the process, it might breed, die of hunger,
+     * or die of old age.
+     * @param field The field currently occupied.
+     * @param newFoxes A list to return newly born foxes.
+     */
+    public void act(List<Animal> newPredators)
+    {
+        incrementAge();
+        incrementHunger();
+        if(isAlive()) {
+            giveBirth(newPredators);            
+            // Move towards a source of food if found.
+            Location newLocation = findFood();
+            if(newLocation == null) { 
+                // No food found - try to move to a free location.
+                newLocation = getField().freeAdjacentLocation(getLocation());
+            }
+            // See if it was possible to move.
+            if(newLocation != null) {
+                setLocation(newLocation);
+            }
+            else {
+                // Overcrowding.
+                setDead();
+            }
+        }
+    }
+
+    /**
+     * Increase the age. This could result in the fox's death.
+     */
+    protected void incrementAge()
+    {
+        int age= getAge();
+        age++;
+      
+        if(age > getMaxAge()) {
+            setDead();
+        }
+    }
+    
+    /**
+     * Look for rabbits adjacent to the current location.
+     * Only the first live rabbit is eaten.
+     * @return Where food was found, or null if it wasn't.
+     */
+    protected Location findFood()
+    {
+        Field field = getField();
+        List<Location> adjacent = field.adjacentLocations(getLocation());
+        Iterator<Location> it = adjacent.iterator();
+        while(it.hasNext()) {
+            Location where = it.next();
+            Object animal = field.getObjectAt(where);
+            if(animal instanceof Badger) {
+                Badger badger = (Badger) animal;
+                if(badger.isAlive()) { 
+                    badger.setDead();
+                    int b = getBadgerFoodValue();
+                    return where;
+                }
+            }
+            else 
+             if(animal instanceof Squirrel) {
+                Squirrel squirrel = (Squirrel) animal;
+                if(squirrel.isAlive()) { 
+                    squirrel.setDead();
+                 int s = getSquirrelFoodValue();
+                    return where;
+                }
+            }
+            else if (animal instanceof Hedgehog) {
+                Hedgehog hedgehog = (Hedgehog) animal;
+                if(hedgehog.isAlive()) { 
+                    hedgehog.setDead();
+                   int h = getHedgehogFoodValue();
+                    return where;
+                }
+        }
+    }
+        return null;
+    }
+    /**
+     * Make this fox more hungry. This could result in the fox's death.
+     */
+    protected void incrementHunger()
+    {
+        int b = getBadgerFoodLevel();
+        int s = getSquirrelFoodLevel();
+        int h = getHedgehogFoodLevel();
+        b--;
+        s--;
+        h--;
+      
+        if((b <= 0 && s<=0)||(b <= 0 && h<=0)||(h <= 0 && s<=0) ){
+            setDead();
+        }
+    }
+    
+    /**
+     * Check whether or not this fox is to give birth at this step.
+     * New births will be made into free adjacent locations.
+     * @param newFoxes A list to return newly born foxes.
+     */
+    abstract void giveBirth(List<Animal> newPredators);
+  
+        
+    /**
+     * Generate a number representing the number of births,
+     * if it can breed.
+     * @return The number of births (may be zero).
+     */
+    protected int breed()
+    {
+        int births = 0;
+        if(canBreed() && rand.nextDouble() <= getBreedingProbability()) {
+            births = rand.nextInt(getMaxLitterSize()) + 1;
+        }
+        return births;
+    }
+
+    /**
+     * A fox can breed if it has reached the breeding age.
+     */
+    protected boolean canBreed()
+    {
+        return getAge() >= getBreedingAge();
+    }
+}
